5eface1cbbc9fa0d194cbb74397784b60aa35f04
Akshatbansal-coder <akshatbansal10c@gmail.com>
2026-09-18 12:38:11 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/design-hashset/submission-0.java
@@ -0,0 +1,28 @@
+class MyHashSet {
+    HashSet<Integer> ans = new HashSet<>();
+
+    public MyHashSet() {
+        
+        
+    }
+    
+    public void add(int key) {
+        ans.add(key);
+    }
+    
+    public void remove(int key) {
+        ans.remove(key);
+    }
+    
+    public boolean contains(int key) {
+       return ans.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
